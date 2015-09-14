cc818f956da5266d29d1e5ecef2bee2a6c9e4aad
jlzizmor <jlzizmor@wpi.edu>
2015-09-14 11:26:05 -0400

    set up and commented a program to test bladder prototype pressures

---
+++ Bladder_Prototype_Testing/extended_bladder_test/extended_bladder_test.ino
@@ -0,0 +1,21 @@
+/*
+	This program is designed to monitor four (4) bladders for constant pressure.
+	Four bladders will be attached to four pressure sensors and teh pressure sensors attached to 4 analong pins on the Arduino.
+	Then all four bladders will be filled to the same pressure.
+	The bladders will then be left alone, the Arduino will monitor their pressure.
+
+	The purpose of this test is to see if any of the bladders can hold pressurized air for an extended period of time.
+	The bladders are expected to remain filled for an extended time on the order of minutes, hours, or even longer.
+	Monitoring the bladders with the Arduino makes this task more convenient and accurate. 
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
